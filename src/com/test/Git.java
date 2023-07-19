55fb6c3f12e93be4e73abf67c64db7d6dc987066
priyaThangaraj26 <priyathangaraj2606@gmail.com>
2023-07-19 17:41:40 +0530

    Created gitProject

---
+++ src/com/test/Git.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Git {
+	
+	private void course1() {
+System.out.println("Java");
+	}
+	
+	private void course2() {
+		System.out.println("Selenium");
+
+	}
+	
+	private void course3() {
+System.out.println("Python");
+	}
+
+}

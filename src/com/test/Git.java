36ff9e0a276ca5698457e475805fa53f134c1495
priyaThangaraj26 <priyathangaraj2606@gmail.com>
2023-07-19 18:04:23 +0530

    Created students fee

--- src/com/test/Git.java
+++ src/com/test/Git.java
@@ -14,5 +14,16 @@ System.out.println("Java");
 	private void course3() {
 System.out.println("Python");
 	}
+	
+	private void studentFee() {
+System.out.println("10000");
+	}
 
+	private void studentFee1() {
+		System.out.println("20000");
+			}
+	
+	private void studentFee2() {
+		System.out.println("30000");
+			}
 }
